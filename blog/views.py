594bd5870e2673bae871136202e8a369ad6bc8bf
mahmoud20212 <occee123@gmail.com>
2022-06-15 13:54:42 +0300

    update

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,30 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+posts = [
+    {
+        'author': 'Mahmoud',
+        'title': 'Blog Post 1',
+        'content': 'Hello World',
+        'date_posted': 'August 28, 2018',
+    },
+    {
+        'author': 'Mahmoud',
+        'title': 'Blog Post 1',
+        'content': 'Mahmoud',
+        'date_posted': 'August 28, 2022',
+    },
+]
+
 
 def index(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/index.html', context)
+
+def about(request):
+    context = {
+        'title': 'About'
+    }
+    return HttpResponse('<h1>Blog about</h1>')
